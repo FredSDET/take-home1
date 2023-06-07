ebd61f70e8a3680e26aa7ac9ec8eeb89f133407a
Fredbabayev <fredbaba.ab@gmail.com>
2023-06-06 18:10:56 -0600

    Gametest assignment passed

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,31 @@
 package fun;
 
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class GameTests {
+    @Test
+    void testDefaultGame() {
+        RestAssured.baseURI = "http://localhost:8080/game";
+        Response response = given().when().get("/game").then().extract().response();
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Playing Sudoku is fun!", response.jsonPath().getString("text"));
+        assertNotNull(response.jsonPath().get("id"));
+        assertTrue(response.jsonPath().get("id") instanceof Integer);
+    }
 
-
+    @Test
+    void testSpecificGame() {
+        RestAssured.baseURI = "http://localhost:8080";
+        Response response = given().queryParam("name", "Chess").when().get("/game").then().extract().response();
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Playing Chess is fun!", response.jsonPath().getString("text"));
+        assertNotNull(response.jsonPath().get("id"));
+        assertTrue(response.jsonPath().get("id") instanceof Integer);
+    }
 }
